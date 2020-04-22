a4ae1a318b72da07c65a2fe922eea3fd17cf9fff
Quentin ANIERE <quentin.aniere@gmail.com>
2020-04-22 21:43:57 +0200

    Ajout de la fonction commandeParseure et de la fonction d'aide

--- app.py
+++ app.py
@@ -11,3 +11,35 @@ def cls():
         os.system("clear")
 #La commande varie selon les os
 
+#On définit la fonction suite pour éviter de se répéter
+def suite():
+    input("Appuyez sur entrée pour continuer \n")
+
+#La fonction d'aide
+def aide():
+    print("Bonzouére je suis la fonction d'aide")
+    suite()
+
+#La fonction qui parse et traite les commandes
+def commandesParseur(commande):
+    
+    #On efface l'écran
+    cls()
+
+    listeCommande = commande.split(" ")
+    #On récupere tout les mots séparés par un espace dans la commande dans une liste
+    #Exemple : "Bonjour toi !" donnera ["Bonjour", "toi", "!"]
+    
+    if listeCommande[0] == "/?":
+    #Commande d'aide
+        aide()
+    else:
+        print('La commande "' + commande + '" est inconnue ! Aide => "/?"')
+        suite()
+
+
+#On demande à l'utilisateur la commande à exécuter, puis on la passe en paramétre de la fonction commandesParseur()
+while True:
+    commande = input("Bienvenue dans $_NOMPROJET ! Tapez /? pour obtenir de l'aide \n >>> ")
+    commandesParseur(commande)
+    cls()
