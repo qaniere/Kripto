dee48afbc15973eaa89e37f57e21a3e56e629f6f
Quentin ANIERE <quentin.aniere@gmail.com>
2020-05-09 17:26:27 +0200

    Suppresion d'une ligne inutile

--- ChiffrementRSA.py
+++ ChiffrementRSA.py
@@ -188,9 +188,3 @@ def inverse(clefPublique, phiDuModuleDeChiffrement):
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
 
     return u1 % phiDuModuleDeChiffrement
-
-
-### Génération des clefs et du module de chiffrement ###
-
-
-moduleDeChiffrement, clefPublique, clefPrivée = génération(16)
