4ee7a36a8044d89eb9c4d188a8d23f5346706d28
Yunidraak <58864041+Yunidraak@users.noreply.github.com>
2020-06-26 18:56:48 +0200

    Nettoyage

--- Modules/Sauvegarde.py
+++ Modules/Sauvegarde.py
@@ -1,26 +1,16 @@
 # coding: utf8
 import time
-import codecs
 import hashlib
 import base64
 from os import makedirs
 
 
-#TODO - J'ai supprimé le message au lecteur car jugé inutile par monsieur Germant
-
-
-#TODO - J'ai aussi supprimer ce genre de commentaires pour la même raison
-### Définition de la fonction de chiffrement ### 
-
-
-def transformationChiffres(chaine):
+def TransformationChiffres(chaine):
 
     """Fonction qui renvoi sous forme de la liste d'entiers correspont chacun à la valeur
     unicode des caratères de la chaine précisée en arguments """ 
-    #TODO - Chiffré ne veut pas dire transformer en chiffres 
-    #TODO - Lis ce lien : https://chiffrer.info/
 
-    liste = list(chaine) #TODO - Fonction évidente, pas besoin d'expliquer
+    liste = list(chaine)
 
     for index in range(len(liste)):
     #On récupere chaque caratères de la liste
@@ -28,15 +18,13 @@ def transformationChiffres(chaine):
         liste[index] = ord(liste[index])
         # ord(x) est la fonction qui pour tout charactère Unicode (sous type str) x renvoie sa valeur Unicode
         # Cette fonction transforme donc le message en une suite de chiffres qui correspond à leur valeur Unicode
-
-    #TODO, Bravo, appart l'utilisation de i au lieu d'index c'est une partie bien commentée
         
     return liste
     #On renvoi la liste des caractères au format 
 
 
 
-def transformationCaratères(ListeUnicode):
+def TransformationCaratères(ListeUnicode):
 
     """Fonction qui convertit la liste de numéros Unicode en une chaine de caractères
     correspondant à chacun des numéros convertis et mis bout à bout""" 
@@ -48,8 +36,12 @@ def transformationCaratères(ListeUnicode):
         
         if ListeUnicode[index] > 1114111 :
 
-            ListeUnicode[index] = ListeUnicode[index] - 1114111 #TODO expliquer pourquoi ce chiffre ?
+            # Les caractères Unicode vont de 0 à 1114111.
+            # Si, à cause du cryptage, la valeur viendrait à quitter cette plage de donnée, cela provoquerait une erreur
+            # On fait donc en sorte que les cactères restent toujours dans cette plage de donnée.
 
+            ListeUnicode[index] = ListeUnicode[index] - 1114111
+            
         elif ListeUnicode[index] < 0 :
 
             ListeUnicode[index] = ListeUnicode[index] + 1114111
@@ -57,8 +49,6 @@ def transformationCaratères(ListeUnicode):
         ListeUnicode[index] = chr(ListeUnicode[index])
         # chr(x) est la fonction qui pour toute valeur Unicode x nous renvoie son charactère Unicode (sous type str)
         # Cette fonction transforme donc la suite de charactère chiffrés sous forme Unicode en message
-        #TODO Bien commenté, GG :)
-
 
     return "".join(ListeUnicode)
 
@@ -69,42 +59,50 @@ def ChiffrementVignère(MotDePasse, chaine) :
     """Fonction qui chiffre une chaine avec un mot de passe comme clef de chiffrement,
     selon la méthode de Vignère"""
 
-    chaine = transformationChiffres(chaine)
+    chaine = TransformationChiffres(chaine)
     #On transforme la chaine en chiffres
 
     MotDePasse = GénérationMotDePasseSécurisé(MotDePasse)
-    MotDePasse = transformationChiffres(MotDePasse)
+    MotDePasse = TransformationChiffres(MotDePasse)
     #On hash le mot de passe, afin d'obtenir un nombre plus élévé de caractères
 
     nombreItérationsVigenère = (len(chaine) // len(MotDePasse))+ 1
-    #TODO Expliquer cette ligne
+    # On calcule le nombre de fois où on aura besoin de répéter le mot de passe
+    # pour pouvoir crypter le message entier selon la méthode Vigenère
 
     longueurItérationVigenère = len(MotDePasse)
-    #TODO Explique cette ligne
+    # On calcule la longueur du mot de passe pour savoir le nombre de
+    # caractères cryptables selon Vigenère en n'utilisant qu'une seule fois
+    # le mot de passe
 
     for x in range(0, nombreItérationsVigenère) :
-    #TODO Expliquer ce qu'est X
+    # x correspond au nombre de fois où l'on devra utiliser le mot de passe
+    # pour appliquer la méthode de cryptage de Vigenère
 
         for y in range(0, longueurItérationVigenère) :
-        #TODO Expliquer ce qu'est y
+        # y correspond à la position du caractère du mot de passe utilisé
+        # au sein du mot de passe pour crypter selon Vigenère
 
             try :
+                
                 chaine[x*longueurItérationVigenère+y] = chaine[x*longueurItérationVigenère+y] + MotDePasse[y]
-            except :
+                # On crypte chaque carractère selon Vigenère, en additionnant les valeurs des caractères du message et du mot de passe
+                
+            except IndexError:
 
-            #TODO MonsieurGermant n'aime pas du tout les except sans execeptions
-            # Exemple except ValueError: OK - except: NON
-
-                a=1 #TODO ????
+                pass
+                # Si on a fini de crypter tous les caractères mais que la boucle de cyptage continue
+                # On ne fait rien
 
     
-    chaine = transformationCaratères(chaine)
+    chaine = TransformationCaratères(chaine)
     chaine = chaine.encode("utf-8")
     chaine = base64.b64encode(chaine)
     chaine = chaine.decode("utf-8")
     #On encode en base64
 
     return str(chaine)
+    # On retourne le message chiffré, sous forme de caratères
 
 
 
@@ -116,35 +114,43 @@ def DéchiffrementVignère(MotDePasse, chaine):
     chaine = chaine.encode("utf-8")
     chaine = base64.b64decode(chaine)
     chaine = chaine.decode("utf-8")
-    chaine = transformationChiffres(chaine)
+    
+    chaine = TransformationChiffres(chaine)
     #On transforme la chaine à chiffrer en liste d'entiers
 
     MotDePasse = GénérationMotDePasseSécurisé(MotDePasse)
-    MotDePasse = transformationChiffres(MotDePasse)
+    MotDePasse = TransformationChiffres(MotDePasse)
     #On hashe le mot le mot de passe, et on le transforme en liste d'entiers
 
     nombreItérationsVigenère = len(chaine) // len(MotDePasse) + 1
-    #TODO Expliquer
+    # On calcule le nombre de fois où on aura besoin de répéter le mot de passe
+    # pour pouvoir crypter le message entier selon la méthode Vigenère
 
     longueurItérationVigenère = len(MotDePasse)
-    #TODO Expliquer
+    # On calcule la longueur du mot de passe pour savoir le nombre de
+    # caractères cryptables selon Vigenère en n'utilisant qu'une seule fois
+    # le mot de passe
 
     for x in range(0, nombreItérationsVigenère) :
-    #TODO Expliquer x et utiliter nom plus équivoque
+    # x correspond au nombre de fois où l'on devra utiliser le mot de passe
+    # pour appliquer la méthode de cryptage de Vigenère
 
         for y in range(0, longueurItérationVigenère):
-        #TODO Expliquer y et utiliter nom plus équivoque
+        # y correspond à la position du caractère du mot de passe utilisé
+        # au sein du mot de passe pour crypter selon Vigenère
 
             try:
+                
                 chaine[x*longueurItérationVigenère+y] = chaine[x*longueurItérationVigenère+y] - MotDePasse[y]
-            except:
-            #TODO MonsieurGermant n'aime pas du tout les except sans execeptions
-            # Exemple except ValueError: OK - except: NON
-                a=1 #TODO ?????
+                # On décrypte chaque carractère selon Vigenère, en soustrayant les valeurs des caractères du mot de passe à ceux du message
 
-     
+            except IndexError:
 
-    return transformationCaratères(chaine)
+                pass
+                # Si on a fini de crypter tous les caractères mais que la boucle de cyptage continue
+                # On ne fait rien
+
+    return TransformationCaratères(chaine)
     #On renvoie la chaine sous formes de caractères
 
 
@@ -152,15 +158,15 @@ def DéchiffrementVignère(MotDePasse, chaine):
 
 def GénérationMotDePasseSécurisé(MotDePasse):
 
-    motDePasseUser = hashlib.sha224(bytes(MotDePasse, "utf-8")).hexdigest()
-    motDePasseConstant = hashlib.sha224(b"q55|R,~gS2.m)RD5d^5N8KcS6?v7Lhb<").hexdigest()
+    MotDePasseUser = hashlib.sha224(bytes(MotDePasse, "utf-8")).hexdigest()
+    MotDePasseConstant = hashlib.sha224(b"q55|R,~gS2.m)RD5d^5N8KcS6?v7Lhb<").hexdigest()
     #On hashe le mot de passe de l'utilisateur + un mot de passe constant pour obtenir un mot bien plus sécurisé
 
-    motDePasseFinal = motDePasseUser + motDePasseConstant
+    MotDePasseFinal = MotDePasseUser + MotDePasseConstant
      
     résultat = []
 
-    for caractère in motDePasseFinal:
+    for caractère in MotDePasseFinal:
     #Pour chaque caratère du hash
 
         try:
@@ -183,15 +189,18 @@ def GénérationMotDePasseSécurisé(MotDePasse):
 
 
 
-def InitialisationSauvegarde(motDePasse):
+def InitialisationSauvegarde(MotDePasse):
 
     """ Qui créé un fichier de sauvegarde au bon emplacement """
 
     try :
-    #On essie de créé un dossier pour stocker les fichiers de sauvegardes
+        
+        #On essie de créé un dossier pour stocker les fichiers de sauvegardes
         makedirs("Messages sauvegardés")
+        
     except FileExistsError :
-    #Si le fichier existe déja
+        
+        #Si le fichier existe déjà on ne fait rien
         pass
 
     NomFichier = "Messages sauvegardés/" + time.strftime("%A-%d-%B-%H.%M.%S")+".KriptoFile"
@@ -201,7 +210,7 @@ def InitialisationSauvegarde(motDePasse):
     HeureDébutConversation = time.strftime("%H:%M:%S")
     #On récupere l'heure et la début de la conversation 
 
-    Annonce = ChiffrementVignère(motDePasse, str(f"Début de la conversation le {DateDébutConversation} à {HeureDébutConversation}."))
+    Annonce = ChiffrementVignère(MotDePasse, str(f"Début de la conversation le {DateDébutConversation} à {HeureDébutConversation}."))
     #On chiffre l'annonce du début du fichier
 
     fichier = open(NomFichier, "w")
@@ -211,20 +220,17 @@ def InitialisationSauvegarde(motDePasse):
     fichier.close()
     # On écrit l'annonce de façon chiffrée puis on saute une ligne
 
-
-    
-
     return NomFichier
 
 
 
 def NouvelleLigne(NomFichier, MotDePasse, chaine):
 
-    ChaineChiffré = ChiffrementVignère(MotDePasse, chaine)
+    ChaineChiffrée = ChiffrementVignère(MotDePasse, chaine)
 
     fichier = open(NomFichier, "a")
 
-    fichier.write(ChaineChiffré)
+    fichier.write(ChaineChiffrée)
     fichier.write("\n")
     fichier.close()
     # On ouvre le fichier de de sauvegarde, on écrit le message chiffré
@@ -233,7 +239,7 @@ def NouvelleLigne(NomFichier, MotDePasse, chaine):
 
 
 
-def LectureSauvegarde(NomFichier, MDP):
+def LectureSauvegarde(NomFichier, MotDePasse):
 
     ListeLignes = []
 
@@ -241,13 +247,14 @@ def LectureSauvegarde(NomFichier, MDP):
 
         for ligne in fichier :
             ListeLignes.append(ligne)
+            #On ajoute chaque ligne du fichier à une liste
 
     fichier.close()
 
     for index in range(len(ListeLignes)):
     #Pour chaque ligne
 
-        ListeLignes[index] = DéchiffrementVignère(MDP, ListeLignes[index])
+        ListeLignes[index] = DéchiffrementVignère(MotDePasse, ListeLignes[index])
         #On déchiffre la ligne
 
         ListeLignes[index] = list(ListeLignes[index])
